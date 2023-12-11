b6d4ac448eed487b72fb79fc455ad7e162986c68
smanrique <37903095+smanrique@users.noreply.github.com>
2023-12-11 20:49:03 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
+//import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
+		/*JFrame frame = new JFrame();
+		frame.setSize(200, 200);
+		frame.setVisible(true); Para poner una ventana emergente.*/
 		Scanner sc = new Scanner(System.in);
 		/*System.out.println("Bienvenido a esta aventura. Pulsa enter para empezar.");
 		String nombreJugador = sc.nextLine();
@@ -14,10 +17,8 @@ public class Main {
 	    TimeUnit.SECONDS.sleep(1);
 	    System.out.println();*/
 		PrimerCamino primerCamino = new PrimerCamino();
-		
-		  System.out.println("Estas apunto de adentrarte a un bosque ¿qué haces?\n1.Adentrarse.\n2.No adentrarse.");
-	        primerCamino.primeraDecision(sc.nextLine()); //Esto envia el valor a la clase
-	        
+		System.out.println("Estas apunto de adentrarte a un bosque.¿Qué haces?\n1.Adentrarse.\n2.No adentrarse.");
+		primerCamino.primeraDecision(sc.nextLine()); //Esto envia el valor a la clase
+		sc.close();
 	}
-
 }
